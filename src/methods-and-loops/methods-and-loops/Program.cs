c67c66afb010c6fc5385f613f3b237630e75c3fe
Russell Carey <russellwcarey@gmail.com>
2020-09-28 20:51:16 -0600

    first commit with visualstudio2019

---
+++ src/methods-and-loops/methods-and-loops/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace methods_and_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
